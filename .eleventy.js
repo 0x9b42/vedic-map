b028671d1775cdc1df4db8c69e13b3e911a65ac0
Mob Elazcona <0x9b42@gmail.com>
2026-08-08 02:03:23 +0700

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/assets/css");
+  eleventyConfig.addPassthroughCopy("src/assets");
   // eleventyConfig.addPassthroughCopy("src/assets/images");
 
   return {
